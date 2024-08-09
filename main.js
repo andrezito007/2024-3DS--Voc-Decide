222963a816070994c1979f61f15648017438e221
andrezito007 <134400745+andrezito007@users.noreply.github.com>
2024-08-09 10:38:40 -0300

    começando javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelecto('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultados');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            'alternativa01',
+            'alternativa02'
+        ]
+    }
+]
