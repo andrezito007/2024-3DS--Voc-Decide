522f206fab2191ad625f775d9627bad525cead15
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-08-15 10:41:55 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -79,23 +79,39 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = '';
 
 function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = '';
     mostraAlternativas();
 }
 
+
 function mostraAlternativas(){
     for( const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement('button')
+        const botaoAlternativas = document.createElement('button');
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener('click', function(){
-            atual++;
-            mostraPergunta();
-        })
-        caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.addEventListener('click', () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas)
+        }
     }
-}
+    function respostaSelecionada(opcaoSelecionada) {
+        const afirmacao = opcaoSelecionada.afirmacao;
+        historiaFinal += afirmacao + ' ';
+        atual++;
+        mostraPergunta()
+    }
+
+    function mostraResultado(){
+        caixaPerguntas.textContent = ' Início do texto...'
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = '';
+    }
 
 mostraPergunta();
